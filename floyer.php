e3fe9e78eb325cbc2d36b8cffd5afe942ad4278a
Sarfraz <sarfraznawaz2005@gmail.com>
2017-08-18 01:48:01 +0500

    updated

---
+++ floyer.php
@@ -0,0 +1,12 @@
+#!/usr/bin/env php
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Sarfraznawaz2005\Floyer\Commands\Deploy;
+use Symfony\Component\Console\Application;
+
+$application = new Application();
+
+$application->add(new Deploy());
+
+$application->run();
